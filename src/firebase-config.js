f8ad575d1a971291862a7f0131805267675c34b3
Traian-Caescu <32572689+Traian-Caescu@users.noreply.github.com>
2024-07-12 10:51:58 +0100

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -4,13 +4,7 @@ import { getAuth, GoogleAuthProvider} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCwhtMJCoZoJn3sBCdqzFl-ITI8nEAH6o",
-  authDomain: "finalproject-5f47b.firebaseapp.com",
-  projectId: "finalproject-5f47b",
-  storageBucket: "finalproject-5f47b.appspot.com",
-  messagingSenderId: "585558374589",
-  appId: "1:585558374589:web:28e211f04c6cd4878b1fd4",
-  measurementId: "G-8B61JD25ZV"
+  
 };
 
 // Initialize Firebase
