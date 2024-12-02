b121f113400ed2508e2384ba3847a81d675a3fb7
Traian-Caescu <32572689+Traian-Caescu@users.noreply.github.com>
2024-12-02 20:46:24 +0000

    Update Roulette.js

--- src/Roulette/Roulette.js
+++ src/Roulette/Roulette.js
@@ -4,7 +4,7 @@ import { db } from '../firebase-config';
 import { collection, addDoc, getDocs, query, orderBy, limit } from "firebase/firestore";
 import { Line, Bar } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
-
+ 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const wheelNumbers = ["0", "32", "15", "19", "4", "21", "2", "25", "17", "34", "6", "27", "13", "36", "11", "30", "8", "23", "10", "5", "24", "16", "33", "1", "20", "14", "31", "9", "22", "18", "29", "7", "28", "12", "35", "3", "26"];
