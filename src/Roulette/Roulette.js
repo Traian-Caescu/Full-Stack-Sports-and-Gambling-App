bec8507cf807272fb729e4ec116b9a58b2c0a739
Traian-Caescu <32572689+Traian-Caescu@users.noreply.github.com>
2024-12-02 20:46:18 +0000

    Update Roulette.js

--- src/Roulette/Roulette.js
+++ src/Roulette/Roulette.js
@@ -11,7 +11,7 @@ const wheelNumbers = ["0", "32", "15", "19", "4", "21", "2", "25", "17", "34", "
 
 const colors = {
     "0": "green", "32": "red", "15": "black", "19": "red", "4": "black", "21": "red",
-    "2": "black", "25": "red", "17": "black", "34": "red", "6": "black", "27": "red",
+    "2": "black", "25": "red", "17": "black", "34": "red", "6": "black", "27": "red", 
     "13": "black", "36": "red", "11": "black", "30": "red", "8": "black", "23": "red",
     "10": "black", "5": "red", "24": "black", "16": "red", "33": "black", "1": "red",
     "20": "black", "14": "red", "31": "black", "9": "red", "22": "black", "18": "red",
